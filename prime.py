01f3bc75f203ac512f731a178ccaf377486728a0
riccardiunix <riccardo.bordon.2014@gmail.com>
2020-11-11 21:42:43 +0100

    fix isPrime function

--- prime.py
+++ prime.py
@@ -4,13 +4,10 @@ from multiprocessing import Pool
 from PIL import Image
 
 def isPrime(n):
-    if n%2 == 0 or n%3 == 0:
-        return 0
     for j in range(6, int(math.sqrt(n)+6)):
         if n%(j-1) == 0 or n%(j+1) == 0:
-            return 0
-    
-    return n-1
+            return 0    
+    return n
 
 if '__main__' == __name__:
     w = 1920
@@ -28,15 +25,18 @@ if '__main__' == __name__:
     i = 6
     while i < n:
         array.append(i-1)
-        if i+1 < n:
-            array.append(i+1)
+        array.append(i+1)
         i += 6
-
-    pool = Pool()
+    if i == n:
+        array.append(i-1)
+    if array[-1] > n:
+        array + array[:-1]
+    
+    pool = Pool(4)
     for i in  pool.map(isPrime, array):
         if (i > 0):
-            arr[i] = fg
+            arr[i-1] = fg
 
     arr = np.reshape(arr, (h, w))
     im = Image.fromarray(arr)
-    im.save("prime_spiral.jpg")
+    im.save("prime_spiral.png")
