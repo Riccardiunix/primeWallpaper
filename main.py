37ed161f52d3df71a8c0bce739218febb12397a9
Riccardiunix <riccardo.bordon.2014@gmail.com>
2023-09-12 20:54:26 +0200

    aggiunta colorazione primi gemelli

--- main.py
+++ main.py
@@ -6,37 +6,45 @@ from PIL import Image
 def isPrime(n):
     for j in range(6, int(math.sqrt(n)+6)):
         if n%(j-1) == 0 or n%(j+1) == 0:
-            return 0    
+            return 0
     return n
 
 if '__main__' == __name__:
-    w = 7680
-    h = 4320
+    w = 1366
+    h = 768
     n = w*h
-    
+
+    fgg = 192
     fg = 128
     bg = 40
-    
+
     arr = np.full((n), bg, np.uint8)
-    for i in [0,1,2,4,6]:
-        arr[i] = fg
-    
+    arr[1] = fg
+    for i in [0,2,4,6]:
+        arr[1] = fgg
+
     array = []
-    i = 6
+    i = 12
     while i < n:
-        array.append(i-1)
-        array.append(i+1)
+        if (i-1) % 5:
+            array.append(i-1)
+        if (i+1) % 5:
+            array.append(i+1)
         i += 6
     if i == n:
         array.append(i-1)
     if array[-1] > n:
         array = array[:-1]
-    
+
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
     for i in  pool.map(isPrime, array):
         if (i > 0):
-            arr[i-1] = fg
+            if (arr[i-3] == fg):
+                arr[i-1] = fgg
+                arr[i-3] = fgg
+            else:
+                arr[i-1] = fg
 
     arr = np.reshape(arr, (h, w))
     im = Image.fromarray(arr)
-    im.save("prime_spiral.png")
+    im.save("primeWallpaper.png")
